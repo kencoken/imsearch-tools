39452b960e5486f14f5efccfd1c512df9f0c4928
Ken Chatfield <ken@robots.ox.ac.uk>
2013-11-26 17:44:45 +0000

    Added new Chrome-user-agent-based google_web engine * Module based on combining AJAX requests from Google's   latest page (Nov 2013) * Old IE-user-agent-based method no longer works   renamed broken module to GoogleOldWebSearch

--- imsearchtools/engines/google_old_web.py
+++ imsearchtools/engines/google_old_web.py
@@ -2,7 +2,6 @@
 
 import requests
 import re
-import math
 from hashlib import md5
 
 from search_client import *
@@ -12,27 +11,29 @@ from api_credentials import *
 #  --------------------------------------------
 
 GOOGLE_WEB_ENTRY = 'http://www.google.com/'
-GOOGLE_WEB_FUNC = 'search'
+GOOGLE_WEB_FUNC = 'images'
 
 ## Search Class
 #  --------------------------------------------
 
-class GoogleWebSearch(requests.Session, SearchClient):
+class GoogleOldWebSearch(requests.Session, SearchClient):
     """Wrapper class for Google Image Search using web interface.
 
     This class does not use any API, but instead extracts results directly from the
-    web search pages (acting as Firefox v25.0).
+    web search pages (acting as Internet Explorer 7.0).
 
-    Created November 2013.
+    Created November 2010, Updated May 2011, confirmed working October 2012.
+
+    Broken as of November 2013.
     """
     
     def __init__(self, async_query=True, timeout=5.0, **kwargs):
-        super(GoogleWebSearch, self).__init__()
+        super(GoogleOldWebSearch, self).__init__()
 
         self.headers.update(kwargs)
         self.timeout = timeout
 
-        self._results_per_req = 100
+        self._results_per_req = 20
         self._supported_sizes_map = {'small': 's',
                                      'medium': 'm',
                                      'large': 'l'}
@@ -46,47 +47,25 @@ class GoogleWebSearch(requests.Session, SearchClient):
     def _fetch_results_from_offset(self, query, result_offset,
                                    aux_params={}, headers={},
                                    num_results=-1):
-        #if num_results == -1:
-        #    num_results = self._results_per_req
-        image_div_pattern = re.compile(r'<div class="rg_di"(.*?)</div>')
-        image_url_pattern = re.compile(r'imgurl=(.*?)&')
-        image_id_pattern = re.compile(r'id":"(.*?):')
+        if num_results == -1:
+            num_results = self._results_per_req
+        image_url_pattern = re.compile(r'/imgres\?imgurl=(.*?)&')
+        image_id_pattern = re.compile(r'tbn:(.*?)"')
             
         try:
-            page_idx = int(math.floor(result_offset/float(self._results_per_req)))
-            page_start = page_idx*self._results_per_req
-            page_offset = result_offset - page_start
-
             # add query position to auxilary parameters
             aux_params['q'] = query
-            aux_params['ijn'] = page_idx  # ijn is the AJAX page index (0 = first page)
-            #aux_params['start'] = page_idx*self._results_per_req # apparently not necessary
+            aux_params['start'] = result_offset
 
             resp = self.get(GOOGLE_WEB_ENTRY + GOOGLE_WEB_FUNC,
                             params=aux_params, headers=headers)
+
             resp_str = resp.text
+            image_urls = image_url_pattern.findall(resp_str)[:(num_results-result_offset)]
+            image_ids = image_id_pattern.findall(resp_str)[:(num_results-result_offset)]
 
-            image_divs = image_div_pattern.findall(resp_str)
-            image_data = []
-            for div in image_divs:
-                image_url_match = image_url_pattern.search(div)
-                image_id_match = image_id_pattern.search(div)
-                if image_url_match and image_id_match:
-                    image_data.append((image_url_match.group(1),
-                                       image_id_match.group(1)))
-
-            # modify returned results list according to input params
-            # (if necessary)
-            if page_offset >= len(image_data):
-                image_data = []
-            else:
-                image_data = image_data[page_offset:]
-            if num_results > 0 and len(image_data) > num_results:
-                image_data = image_data[:num_results]
-
-            # package for output
             resp_dict = [{'url': item[0],
-                          'image_id': md5(item[1]).hexdigest()} for item in image_data]
+                          'image_id': md5(item[1]).hexdigest()} for item in zip(image_urls, image_ids)]
 
             return resp_dict
         except requests.exceptions.RequestException:
@@ -105,15 +84,12 @@ class GoogleWebSearch(requests.Session, SearchClient):
             tbs_list.append('itp:%s' % style)
 
         tbs_str = ','.join(tbs_list)
-
-        # prepare shared parameters
+        
         aux_params = {}
-        aux_params['tbm'] = 'isch' #image search mode
-        aux_params['ijn'] = 0      # causes AJAX request contents only to be returned
         if tbs_str:
             aux_params['tbs'] = tbs_str
             
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:25.0) Gecko/20100101 Firefox/25.0'}
+        headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
 
         # do request
         results = self._fetch_results(query,
